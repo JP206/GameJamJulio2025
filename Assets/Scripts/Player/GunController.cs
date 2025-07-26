576bd332adcb76d723a1314b1e72ab8a51962a97
JP206 <juanpecose@hotmail.com>
2025-07-26 16:14:56 -0300

    balas y cositas

--- Assets/Scripts/Player/GunController.cs
+++ Assets/Scripts/Player/GunController.cs
@@ -1,10 +1,12 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class GunController : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
-    [SerializeField] private float bulletSpeed = 10f;
+    [SerializeField] private float bulletSpeed = 10f, bulletAmmo;
+    [SerializeField] TextMeshProUGUI ammoText;
 
     private void Update()
     {
@@ -16,17 +18,35 @@ public class GunController : MonoBehaviour
 
     private void FireBullet()
     {
-        GameObject bullet = BulletPool.Instance.GetBullet();
-        bullet.transform.position = firePoint.position;
+        if (bulletAmmo > 0)
+        {
+            bulletAmmo--;
+            ammoText.text = "Ammo: " + bulletAmmo.ToString();
 
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 fireDirection = (mousePos - (Vector2)firePoint.position).normalized;
+            GameObject bullet = BulletPool.Instance.GetBullet();
+            bullet.transform.position = firePoint.position;
 
-        float angle = Mathf.Atan2(fireDirection.y, fireDirection.x) * Mathf.Rad2Deg;
-        bullet.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 fireDirection = (mousePos - (Vector2)firePoint.position).normalized;
 
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.linearVelocity = fireDirection * bulletSpeed;
+            float angle = Mathf.Atan2(fireDirection.y, fireDirection.x) * Mathf.Rad2Deg;
+            bullet.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.linearVelocity = fireDirection * bulletSpeed;
+        }
+        else
+        {
+            // sonido de que no puede disparar
+        }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ammo"))
+        {
+            bulletAmmo += 10;
+            ammoText.text = "Ammo: " + bulletAmmo.ToString();
+        }
+    }
 }
