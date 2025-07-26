164f9c07c225e044ddd4d2b0fadc314e01462732
JP206 <juanpecose@hotmail.com>
2025-07-26 11:53:55 -0300

    wave manager

--- Assets/Scripts/Waves/WaveManager.cs
+++ Assets/Scripts/Waves/WaveManager.cs
@@ -1,13 +1,18 @@
+using System.Collections;
 using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
+    [SerializeField] Transform[] spawnPoints;
+
     EnemyPool enemyPool;
     int round = 1, enemiesToSpawn = 5, bossesToSpawn = 1, deadEnemies = 0, bossWaves = 3;
 
     void Start()
     {
         enemyPool = GetComponent<EnemyPool>();
+
+        StartWave();
     }
 
     public void NotifyDeath()
@@ -16,9 +21,17 @@ public class WaveManager : MonoBehaviour
 
         if (deadEnemies == enemiesToSpawn)
         {
-            ScaleDifficulty();
-            StartWave();
+            StartCoroutine(TimeBetweenWaves());
         }
+    }
+
+    IEnumerator TimeBetweenWaves()
+    {
+        ScaleDifficulty();
+
+        yield return new WaitForSeconds(3);
+
+        StartWave();
     }
 
     void ScaleDifficulty()
@@ -34,16 +47,33 @@ public class WaveManager : MonoBehaviour
 
     void StartWave()
     {
+        deadEnemies = 0;
+
         for (int i = 0; i < enemiesToSpawn; i++)
         {
-            enemyPool.GetEnemy();
-            // cambiar la posicion donde spawnean
+            GameObject enemyInstance = enemyPool.GetEnemy();
+            enemyInstance.GetComponent<EnemyController>().SetWaveManager(this);
+            enemyInstance.transform.position = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
+            RandomizePosition(enemyInstance.transform);
         }
 
         if (round % bossWaves == 0)
         {
-            enemyPool.GetBoss();
-            //cambiar la posicion donde spawnea
+            for (int i = 0; i < bossesToSpawn; i++)
+            {
+                GameObject bossInstance = enemyPool.GetBoss();
+                bossInstance.GetComponent<EnemyController>().SetWaveManager(this);
+                bossInstance.transform.position = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
+                RandomizePosition(bossInstance.transform);
+            }
         }
+    }
+
+    void RandomizePosition(Transform transform)
+    {
+        float randomX = Random.Range(0, 3f);
+        float randomY = Random.Range(0, 3f);
+
+        transform.position = new Vector2(transform.position.x + randomX, transform.position.y + randomY);
     }
 }
