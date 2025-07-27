5a9bbf2d943dd3534e5da1a3c6832b8477512082
SebasRodJos <132218836+SebasRodJos@users.noreply.github.com>
2025-07-26 21:54:46 -0300

    Main Menu

--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -1,15 +1,71 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class ChangeScenes : MonoBehaviour
 {
+    [SerializeField] private float delayBeforeSceneChange = 1f;
+    [SerializeField] private GameObject objectToActivateAfterAnimation;
+
     public void ChangeScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(ChangeSceneAfterDelay(sceneName));
     }
 
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    private IEnumerator ChangeSceneAfterDelay(string sceneName)
+    {
+        yield return new WaitForSeconds(delayBeforeSceneChange);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void AnimateThenChangeScene(GameObject buttonObject, string sceneName)
+    {
+        StartCoroutine(AnimateAndChangeScene(buttonObject, sceneName));
+    }
+
+    private IEnumerator AnimateAndChangeScene(GameObject buttonObject, string sceneName)
+    {
+        yield return AnimateClickEffect(buttonObject);
+        yield return new WaitForSeconds(delayBeforeSceneChange);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void AnimateOnly(GameObject buttonObject)
+    {
+        StartCoroutine(AnimateClickEffect(buttonObject));
+    }
+
+    public void AnimateThenActivate(GameObject buttonObject, GameObject objectToActivate)
+    {
+        StartCoroutine(AnimateAndActivate(buttonObject, objectToActivate));
+    }
+
+    private IEnumerator AnimateAndActivate(GameObject buttonObject, GameObject objectToActivate)
+    {
+        yield return AnimateClickEffect(buttonObject);
+        yield return new WaitForSeconds(delayBeforeSceneChange);
+        if (objectToActivate != null)
+            objectToActivate.SetActive(true);
+    }
+
+    // NUEVO: llama a un objeto precargado desde el Inspector
+    public void AnimateAndActivateFromSerialized(GameObject buttonObject)
+    {
+        StartCoroutine(AnimateAndActivate(buttonObject, objectToActivateAfterAnimation));
+    }
+
+    private IEnumerator AnimateClickEffect(GameObject buttonObject)
+    {
+        Vector3 originalScale = buttonObject.transform.localScale;
+        Vector3 pressedScale = originalScale * 0.9f;
+
+        buttonObject.transform.localScale = pressedScale;
+        yield return new WaitForSeconds(0.1f);
+        buttonObject.transform.localScale = originalScale;
     }
 }
